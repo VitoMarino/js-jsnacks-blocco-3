0c714ce5eeefeb79ff716c7a80ac0cef39e41d22
VitoMarino <marino.vito2001@gmail.com>
2024-05-14 17:53:51 +0200

    snack1

--- array-lista/script.js
+++ array-lista/script.js
@@ -1,18 +1,16 @@
-const fruitsNelFrigo = ['banana', 'mela', 'pera', 'ciligia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+const fruitsNelFrigo = [];
 const fruitsAggiunto = ['pesca'];
-const arraytre = fruitsNelFrigo.concat(fruitsAggiunto);
-let memoriaDiInformazioni = '';
+const frutti = fruitsMelFrigo.concat(fruitsAggiunto);
+let hoTrovatoIlCocomero = false;
 
-// length è una proprietà degli ARRAY. Ci da la sua lunghezza. Usarla nei cicli.
-for (i = 0; i < arraytre.length; i++) {
-    // MEMORIZZO UNA INFORMAZIONE CHE MI DICA SE IL COCOMERO C'E' OPPURE NO
-    memoriaDiInformazioni === arraytre['cocomero']
+for (let i = 0; i < frutti.length; i++) {
+    if (frutti[i] === 'cocomero') {
+        hoTrovatoIlCocomero = true;
+    }
 }
 
-if (arraytre === memoriaDiInformazioni) {
-    console.log("Trovato! Devo solo preparare il cocktail.")
+if (hoTrovatoIlCocomero === true) {
+    console.log("Trovato! Devo solo preparare il cocktail")
 } else {
     console.log("Oh no, devo uscire a comprare il cocomero!")
 }
-
-// USO QUELL'INFORMAZIONE MEMORIZZATA PER TRARRE DELLE CONCLUSIONI
--- array_tail/script.js
+++ array_tail/script.js
@@ -1,9 +1,10 @@
 const question = Number.parseInt(prompt('Quanti elementi deve avere il tuo ARRAY?'));
 
-const numeriCasuali = [Math.floor(Math.random() * 100)+ 1];
+const numeriCasuali = Math.floor(Math.random() * 100)+ 1;
 
 const array = [];
 
 for (let i = 0; i < question; i++) {
+    
     console.log(question, array)
 }
