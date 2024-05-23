3120696e09eaf9ac6cd2cc4340d9c6bc805684f3
VitoMarino <marino.vito2001@gmail.com>
2024-05-23 12:14:46 +0200

    push

---
+++ JS-object 2/script.js
@@ -0,0 +1,13 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+let array = ['autoUno', 'autoDue', 'autoTre', 'autoQuattro', 'autoCinque', 'autoSei', 'autoSette', 'autoOtto', 'autoNove', 'autoDieci', 'autoUndici', 'autoDodici', 'autoTredici', 'autoQuattordici', 'autoQuindici'];
+let auto = [
+    {
+        marca: 
+        modello:
+        alimentazione:
+    }
+]
